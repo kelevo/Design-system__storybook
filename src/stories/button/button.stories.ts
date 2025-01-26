c2b7b7bb192aaa11534708fe3866be7808d136ba
Patrick Hernandez <patrickjhonatanh@gmail.com>
2025-01-25 18:30:42 -0600

    Historias de storybook completas

--- src/stories/button/button.stories.ts
+++ src/stories/button/button.stories.ts
@@ -1,11 +1,29 @@
-import { Meta, StoryObj } from '@storybook/html';
+import { Meta, StoryObj, Decorator } from '@storybook/html';
 import { createButton } from './Button';
 import type { ButtonProps } from './Button';
+
+const decorator: Decorator = (story) => {
+  const container = document.createElement('div');
+  container.style.margin = '3rem';
+
+  const storyContent = story();
+  if (storyContent instanceof HTMLElement) {
+    container.appendChild(storyContent);
+  } else if (typeof storyContent === 'string') {
+    container.innerHTML = storyContent;
+  }
+
+  return container;
+};
 
 export default {
 
   title: 'Design System/Atoms/Button',
   tags: ['Design Systems', 'Atoms'],
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [decorator],
   render: (args) => {
     return createButton(args);
   },
